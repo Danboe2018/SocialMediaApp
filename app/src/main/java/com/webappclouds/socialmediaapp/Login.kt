76b847bbd196c4f70e8359ac2d2e2da8fdf883b1
Dan Boehmke <Daniel@webappclouds.com>
2019-10-25 15:13:29 -0400

    Lint Fix

--- app/src/main/java/com/webappclouds/socialmediaapp/Login.kt
+++ app/src/main/java/com/webappclouds/socialmediaapp/Login.kt
@@ -3,12 +3,17 @@ package com.webappclouds.socialmediaapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        ivImagePerson.setOnClickListener( View.OnClickListener {
+            
+        })
     }
 
     fun butLogin(view: View) {
