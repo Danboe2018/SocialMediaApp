f90d916cd8c10f57738101688172ab881daab10d
Dan Boehmke <Daniel@webappclouds.com>
2019-10-25 15:06:22 -0400

    Initial Commit

---
+++ app/src/main/java/com/webappclouds/socialmediaapp/Login.kt
@@ -0,0 +1,17 @@
+package com.webappclouds.socialmediaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun butLogin(view: View) {
+
+    }
+}
