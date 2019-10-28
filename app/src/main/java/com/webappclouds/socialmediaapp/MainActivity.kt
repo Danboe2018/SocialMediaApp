8230dcf4173e2b91e143619938bb5c57d14ba35f
Dan Boehmke <Daniel@webappclouds.com>
2019-10-28 13:41:15 -0400

    Upload image

--- app/src/main/java/com/webappclouds/socialmediaapp/MainActivity.kt
+++ app/src/main/java/com/webappclouds/socialmediaapp/MainActivity.kt
@@ -4,18 +4,22 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.add_ticket.view.*
+import kotlinx.android.synthetic.main.tweets_ticket.view.*
 import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
@@ -46,6 +50,8 @@ class MainActivity : AppCompatActivity() {
 
         var adapter = MyTweetAdapter(this, listTweets)
         lvTweets.adapter = adapter
+
+        loadPost()
     }
 
     inner class MyTweetAdapter : BaseAdapter {
@@ -58,8 +64,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-            var mytweet = listNotesAdpater[p0]
-            if (mytweet.tweetPersonUID.equals("add")) {
+            var myTweet = listNotesAdpater[p0]
+            if (myTweet.tweetPersonUID.equals("add")) {
                 var myView = layoutInflater.inflate(R.layout.add_ticket, null)
                 myView.iv_attach.setOnClickListener(View.OnClickListener {
                     loadImage()
@@ -79,6 +85,29 @@ class MainActivity : AppCompatActivity() {
 
             } else {
                 var myView = layoutInflater.inflate(R.layout.tweets_ticket, null)
+                myView.txt_tweet.text = myTweet.tweetText
+                Picasso.get().load(myTweet.tweetImageURL).into(myView.tweet_picture)
+                myRef.child("Users").child(myTweet.tweetPersonUID!!)
+                    .addValueEventListener(object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            try {
+                                var td = dataSnapshot!!.value as HashMap<String, Any>
+                                for (key in td.keys) {
+                                    var userInfo = td[key] as String
+                                    if (key.equals("ProfileImage")) {
+                                        Picasso.get().load(userInfo)
+                                            .into(myView.picture_path)
+                                    } else {
+                                        myView.txtUserName.text = userInfo
+                                    }
+                                }
+                            } catch (ex: Exception) {
+                            }
+                        }
+
+                        override fun onCancelled(p0: DatabaseError) {
+                        }
+                    })
 
                 return myView
             }
@@ -127,10 +156,10 @@ class MainActivity : AppCompatActivity() {
     var downloadURL: String? = ""
     fun UploadImage(bitmap: Bitmap) {
         listTweets.add(0, Ticket("0", "him", "url", "loading"))
-        adapter!!.notifyDataSetChanged()
+//        adapter!!.notifyDataSetChanged()
 
         val storage = FirebaseStorage.getInstance()
-        val storgaRef = storage.getReferenceFromUrl("gs://gameudemy.appspot.com")
+        val storgaRef = storage.getReferenceFromUrl("gs://socialmediaapp-264ae.appspot.com")
         val df = SimpleDateFormat("ddMMyyHHmmss")
         val dataobj = Date()
         val imagePath = SplitString(myEmail!!) + "." + df.format(dataobj) + ".jpg"
@@ -140,18 +169,46 @@ class MainActivity : AppCompatActivity() {
         val data = baos.toByteArray()
         val uploadTask = ImageRef.putBytes(data)
         uploadTask.addOnFailureListener {
-            Toast.makeText(applicationContext, "fail to upload", Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, "Failed to upload", Toast.LENGTH_LONG).show()
         }.addOnSuccessListener { taskSnapshot ->
-
             downloadURL = taskSnapshot.storage.downloadUrl.toString()!!
             listTweets.removeAt(0)
             adapter!!.notifyDataSetChanged()
-
         }
     }
 
     fun SplitString(email: String): String {
         val split = email.split("@")
         return split[0]
+    }
+
+    fun loadPost() {
+        myRef.child("posts")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    try {
+                        listTweets.clear()
+                        listTweets.add(Ticket("0", "him", "url", "add"))
+                        listTweets.add(Ticket("0", "him", "url", "ads"))
+                        var td = dataSnapshot!!.value as HashMap<String, Any>
+                        for (key in td.keys) {
+                            var post = td[key] as HashMap<String, Any>
+                            listTweets.add(
+                                Ticket(
+                                    key,
+                                    post["text"] as String,
+                                    post["postImage"] as String
+                                    , post["userUID"] as String
+                                )
+                            )
+                        }
+                        adapter!!.notifyDataSetChanged()
+                    } catch (ex: Exception) {
+                    }
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+                }
+            })
     }
 }
